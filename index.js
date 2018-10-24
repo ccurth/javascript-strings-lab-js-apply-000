7b7c8735fcb2466ad1dbf4a8680d8698ee370fc9
thoughtful-calculator-1463 <ccurth@gmail.com>
2018-10-24 01:22:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 var greeting = "Hello, everybody!";
  
 var specialGuest = "Neil deGrasse Tyson";
-
-var greetingSpecialGuest = "Hello,";
  
-var greetSpecialGuest = `${greetingSpeciaGuest} + ${specialGuest} + !`;
+var greetSpecialGuest = "" + specialGuest + "!";
  
 var topic = "space";
  
