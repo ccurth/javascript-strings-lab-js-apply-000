ea363f2fa4c1bb2b504272f8fa918c020933eaa9
thoughtful-calculator-1463 <ccurth@gmail.com>
2018-10-24 01:20:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
  
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetingSpecialGuest
+var greetingSpecialGuest = "Hello,"
  
 var greetSpecialGuest = `${greetingSpeciaGuest} + specialGuest + !`
  
